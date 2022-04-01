6f18bb2cc453fbdc715f70f6f2d7937a2226ef47
Glitch Phoenix <78001268+GlitchPhoenix98@users.noreply.github.com>
2022-04-01 00:48:19 -0400

    Added the PC script.

---
+++ ucp.py
@@ -0,0 +1,23 @@
+import os
+import time
+import sys
+sys.tracebacklimit = 0
+os.system("clear")
+os.system("figlet UCP")
+print("1. Neofetch")
+print("2. Htop")
+print("3. Reboot")
+print("4. Terminal")
+opt = input ()
+if opt == ('1'):
+    os.system("clear")
+    os.system("neofetch")
+    time.sleep(3)
+    os.system("clear")
+    time.sleep(0.5)
+    os.system("python test.py")
+if opt == ('2'):
+    os.system("htop")
+    os.system("python test.py")
+if opt == ('3'):
+    os.system("reboot")
