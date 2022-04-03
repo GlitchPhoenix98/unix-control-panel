eff3a81e697225437b8e04e10828f6594b04c1ba
federatives <102924993+federatives@users.noreply.github.com>
2022-04-03 13:09:00 -0700

    Update installpc.sh

--- installpc.sh
+++ installpc.sh
@@ -1,2 +1,8 @@
-# This is an installation script.
-cp ucp.py /usr/local/bin/ucp
+#!/bin/sh
+#
+# Export a variable that modifies the path where it installs to, making it more flexible
+UCPINSTALL=/usr/local
+cp ucp.py ${UCPINSTALL}/bin/ucp
+chmod +x ${UCPINSTALL}/bin/ucp
+# Unset the variable
+unset UCPINSTALL
